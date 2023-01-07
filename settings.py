e54887c62b180d456f256444e58c1c0f386db0bc
Fedor Gornostaev <102575921+P-or-G@users.noreply.github.com>
2023-01-07 13:48:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-SIZE = WIDTH, HEIGHT = 1920, 1080
-CELL_SIDE = 120
-CELL_VERT_NUM = 9
-CELL_HOR_NUM = 16
+SIZE = WIDTH, HEIGHT = 1024, 1024
+CELL_SIDE = 16
+CELL_VERT_NUM = HEIGHT // CELL_SIDE
+CELL_HOR_NUM = WIDTH // CELL_SIDE
+ass_cell_y_crds = ['grass_1.png', 'grass_2.png', 'grass_3.png', 'grass_4.png']
