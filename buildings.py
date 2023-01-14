cc3343b1b5f89d122d5bdc97ab0b5cd1f4ca6f87
Володя <118829074+Riminis@users.noreply.github.com>
2023-01-14 16:46:58 +0300

    Update buildings.py

--- buildings.py
+++ buildings.py
@@ -1,31 +1,263 @@
 from settings import *
 import pygame
-import os
-import sys
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('assets', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+from cells import *
+from resources import *
+from resources import BREAD, WOOD, STONE, IRON, MONEY, WHEAT, IRON_ORE, GOLD_ORE
 
 
 class MainHall(pygame.sprite.Sprite):
-    def __init__(self, group, x, y):
+    def __init__(self, group, board):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно !!!
         super().__init__(group)
         self.image = load_image('main_hall_lvl1.png')
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        while True:
+            self.rect.x, self.rect.y = random.randrange(0, WIDTH + 448, 16), random.randrange(16, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            try:
+                for i in range(0, 65, 16):
+                    if board.get_cell(x + i, y).im in safe_types:
+                        f1 = True
+                    else:
+                        f1 = False
+                        break
+                for i in range(0, 65, 16):
+                    if board.get_cell(x, y + i).im in safe_types:
+                        f2 = True
+                    else:
+                        f2 = False
+                        break
+                if f1 and f2:
+                    break
+            except:
+                pass
+
+    def select(self, x, y):
+        if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+            print("WIP")
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+
+
+class Mill(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('mill_1.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        while True:
+            self.rect.x, self.rect.y = random.randrange(16, WIDTH, 16), random.randrange(16, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            try:
+                if board.get_cell(x, y).im in safe_types:
+                    break
+            except:
+                pass
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+        if self.tick % 50 == 0:
+            self.image = load_image('mill_1.png')
+        elif self.tick % 50 == 13:
+            self.image = load_image('mill_2.png')
+        elif self.tick % 50 == 25:
+            self.image = load_image('mill_3.png')
+        elif self.tick % 50 == 38:
+            self.image = load_image('mill_4.png')
+
+
+class Ferma(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('mill_1.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        while True:
+            self.rect.x, self.rect.y = random.randrange(16, WIDTH, 16), random.randrange(16, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            try:
+                if board.get_cell(x, y).im in safe_types:
+                    break
+            except:
+                pass
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+        if self.tick % 200 == 0:
+            self.image = load_image('ferma_1.png')
+        elif self.tick % 200 == 50:
+            self.image = load_image('ferma_2.png')
+        elif self.tick % 200 == 100:
+            self.image = load_image('ferma_3.png')
+        elif self.tick % 200 == 150:
+            self.image = load_image('ferma_4.png')
+
+
+class Sawmill(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image(sawmill_types[random.randint(0, 1)])
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        while True:
+            self.rect.x, self.rect.y = random.randrange(16, WIDTH, 16), random.randrange(16, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            try:
+                if board.get_cell(x, y).im in safe_types:
+                    break
+            except:
+                pass
+
+    def select(self, x, y):
+        if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+            print('WIP')
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+
+
+class Quarry(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('quarry.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        # ----------------------------------------------------------------------------------------------------------------------
+        #   ПЕРЕМЕННАЯ ВРЕМЕННА И ПОДЛЕЖИТ РАСТРЕЛУ!!!
+        self.quarry = 0
+        # ----------------------------------------------------------------------------------------------------------------------
+        while True:
+            self.rect.x, self.rect.y = random.randrange(0, WIDTH, 16), random.randrange(0, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            x1 = x + 16
+            y1 = y + 16
+            if board.get_cell(x, y).im in safe_types and board.get_cell(x1, y).im in safe_types \
+                    and board.get_cell(x, y1).im in safe_types and board.get_cell(x1, y1).im in safe_types:
+                break
+
+    def select(self, x, y):
+        if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+            print('WIP')
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+
+
+class Bread(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('bread.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        # ----------------------------------------------------------------------------------------------------------------------
+        #   ПЕРЕМЕННАЯ ВРЕМЕННА И ПОДЛЕЖИТ РАСТРЕЛУ!!!
+        self.bread = 0
+        # ----------------------------------------------------------------------------------------------------------------------
+        while True:
+            self.rect.x, self.rect.y = random.randrange(0, WIDTH, 16), random.randrange(0, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            x1 = x + 16
+            y1 = y + 16
+            if board.get_cell(x, y).im in safe_types and board.get_cell(x1, y).im in safe_types \
+                    and board.get_cell(x, y1).im in safe_types and board.get_cell(x1, y1).im in safe_types:
+                break
+
+    def select(self, x, y):
+        if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+            print('WIP')
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+
+
+class Foundry(pygame.sprite.Sprite):
+        def __init__(self, group, board):
+            super().__init__(group)
+            self.image = load_image('foundry.png')
+            self.rect = self.image.get_rect()
+            self.board = board
+            self.tick = 0
+            self.flag = True
+
+            # ----------------------------------------------------------------------------------------------------------------------
+            #   ПЕРЕМЕННАЯ ВРЕМЕННА И ПОДЛЕЖИТ РАСТРЕЛУ!!!
+            self.foundry = 0
+            # ----------------------------------------------------------------------------------------------------------------------
+            while True:
+                self.rect.x, self.rect.y = random.randrange(0, WIDTH, 16), random.randrange(0, HEIGHT, 16)
+                x = self.rect.x
+                y = self.rect.y
+                x1 = x + 16
+                y1 = y + 16
+
+                if board.get_cell(x, y).im in safe_types and board.get_cell(x1, y).im in safe_types \
+                        and board.get_cell(x, y1).im in safe_types and board.get_cell(x1, y1).im in safe_types:
+                    break
+
+        def select(self, x, y):
+            if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+                print('WIP')
+
+        def update(self, *args, **kwargs):
+            self.tick += 1
+
+
+class Mine(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('mine.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
+        # ----------------------------------------------------------------------------------------------------------------------
+        #   ПЕРЕМЕННАЯ ВРЕМЕННА И ПОДЛЕЖИТ РАСТРЕЛУ!!!
+        self.mine = 0
+        # ----------------------------------------------------------------------------------------------------------------------
+        while True:
+            self.rect.x, self.rect.y = random.randrange(0, WIDTH, 16), random.randrange(0, HEIGHT, 16)
+            x = self.rect.x
+            y = self.rect.y
+            x1 = x + 16
+            y1 = y + 16
+            if board.get_cell(x, y).im in safe_types and board.get_cell(x1, y).im in safe_types \
+                    and board.get_cell(x, y1).im in safe_types and board.get_cell(x1, y1).im in safe_types:
+                break
+
+    def select(self, x, y):
+        if self.rect.x <= x <= self.rect.x + 32 and self.rect.y <= y <= self.rect.y + 32:
+            print('WIP')
+
+    def update(self, *args, **kwargs):
+        self.tick += 1
+
+
+class Berater(pygame.sprite.Sprite):
+    def __init__(self, group, board):
+        super().__init__(group)
+        self.image = load_image('berater.png')
+        self.rect = self.image.get_rect()
+        self.board = board
+        self.tick = 0
+        self.flag = True
